d758bcfdf924339bda27be173a669af8eaab424d
Arjun Downs <abdowns@gmail.com>
2014-12-05 14:43:29 -0500

    Added sign up / in / out links to layout
    * In order to rout to the correct pages with these links:
    *   - Generated devise User controllers
    *   - Added controllers argument to routes
    * Cleaned up form syntax within Sign up / in / out pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resource :home, only: [:index]
   root to: "home#index"
+  resource :home, only: [:index]
 
-  devise_for :users
+  devise_for :users, controllers: {
+                                   sessions: "users/sessions",
+                                   registrations: "users/registrations",
+                                   passwords: "users/passwords"
+                                  }
 end
